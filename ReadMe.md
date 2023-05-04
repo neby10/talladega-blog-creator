8ce6ecc93c3e4377e98edbe30b227466909a8472
Nicholas Eby <49201079+neby10@users.noreply.github.com>
2023-05-04 16:37:42 -0400

    Update #2 using Flask-Login
    -basic functionality of login and logout appear to be working (will expand soon)
    -added ReadMe tips for updating model if need be (likely will need to)
    -further worked on admin page and simplified TBC page to expand upon later

--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,7 @@
 Name: Nick Eby
 Created: May 2, 2023
 
-# Project Name: Python Flask Blog
+# Project Name: Talladega Blog Creator
 
 ## Description:
 This is a fully functional blog web application using Flask.
@@ -22,14 +22,36 @@ HTML, CSS, and Bootstrap for front-end development
 
 
 
-A Python interactive shell will be opened. This special shell runs commands in the context of your Flask application, so that the Flask-SQLAlchemy functions you’ll call are connected to your application.
+A Python interactive shell will be opened when 'flask shell' is run. 
+This special shell runs commands in the context of your Flask application, 
+so that the Flask-SQLAlchemy functions you’ll call are connected to your application.
 $ flask shell
->>> from app import db, User, Post, Category, Comment
+>>> from app import db, User, Post, Comment
 >>> db.create_all()
 
+To delete a database:
+>>> db.drop_all()
+
+When the model of your database changes, you must do a few things to ensure the data
+in the database is not lost.
+1. Update the model in your Flask application code.
+2. Use the Flask-SQLAlchemy CLI to generate a migration script. The CLI compares the current database schema to the new model and generates a migration script that will update the schema to match the new model.
+    I. Make sure you have Flask-Migrate installed in your project
+    II. Initialize Flask-Migrate in your project directory. This creates a migrations directory in your projects folder.
+            >>> flask db init
+    III. Run command to generate a migration script. This will generate a migration script in the migrations directory that includes the necessary changes to your database schema.
+            >>> flask db migrate -m "Add ___ to User model"
+    IV. Apply the migration to your database.
+            >>> flask db upgrade
+3. Review and modify the migration script as necessary. The generated script may not always be perfect, and you may need to tweak it to handle edge cases or other special circumstances.
+4. Apply the migration script to the database using the Flask-SQLAlchemy CLI. The CLI runs the migration script and modifies the database schema to match the new model.
+    I. This command applies all the migration scripts that have not yet been applied to the database, bringing the schema up to date with the current model.
+            >>> flask db upgrade
+    II. To revert the changes, you can use this command to roll back the changes.
+            >>> flask db downgrade
 
 
-Requirements
+## Requirements
 -Admin
     -Post, Edit, Delete Content on a User's Page
     -Access All Items in Admin Toolbar
@@ -44,7 +66,7 @@ Requirements
 
 Features: pagination, search, authentication
 
-Action Plan:
+## Action Plan:
 
 1. Set up the Flask environment and create the project structure.
 Install Flask and required packages using pip
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, url_for, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask import Flask, request, url_for, redirect, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
+
 
 # Instantiate Flask Application
 app = Flask(__name__)
@@ -9,13 +11,24 @@ app = Flask(__name__)
 # Connect Application with SQLAlchemy Database
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Login Manager contains code that lets application and Flask-Login work together
+# login_manager = LoginManager()
+# Configure it to app
+# login_manager.init_app(app)
+
+# TODO: CHANGE SECRET KEY AND KEEP IT VERY SECRET!!!
+# secret key used for Flask-Login sessions
+app.config['SECRET_KEY'] = "secret-key-really-secret"
+# sets config variable to sqlite base directory
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
+# disables an unnecessary feature of SQLAlchemy that tracks modifications to objects and generates warnings
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+    
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
@@ -26,19 +39,28 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+    @property
+    def is_authenticated(self):
+        return True
+    
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.user_id)
+
 class Post(db.Model):
     post_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
     content = db.Column(db.Text)
     author_id = db.Column(db.Integer) # foreign key to user.user_id
-    category_id = db.Column(db.Integer) #foreing key to category.category_id
-    # published_at = db.Column()
+    category_id = db.Column(db.Integer) # foreign key to category.category_id
     create_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-
-class Category(db.Model):
-    category_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
 class Comment(db.Model):
     comment_id = db.Column(db.Integer, primary_key=True)
@@ -54,32 +76,40 @@ class Comment(db.Model):
 # User(username="user4", email="user4@gmail.com", password="password4", bio="bio 4")
 # User(username="user5", email="user5@gmail.com", password="password5", bio="what up bitches")
 
+# Create login manager to handle authentication and session management
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-# TODO: Modularize routes into routes.py
-# from app import routes
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# TODO: Modularize routes into routes.py # from app import routes
+
 @app.route("/")
 def index():
-    users = User.query.all()
-    return render_template("index.html", users=users)
+    # If logged in, pass in user to render_template function
+    return render_template("index.html")
 
-@app.route("/admin", methods=("GET", "POST", "DELETE"))
-def admin():
-    users = User.query.all()
-    # if admin clicks delete user
-        # user should be deleted from database
-    return render_template("admin.html", users=users)
+@app.route("/delete_user/<int:user_id>", methods=["POST"])
+def delete_user(user_id):
+    if request.method == "POST":
+        user = User.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+    # users = User.query.all()
+    # return render_template("admin.html", users=users)
 
 @app.route("/<int:user_id>/")
 def user(user_id):
     user = User.query.get_or_404(user_id)
     return render_template("user.html", user=user)
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
 
 @app.route("/create", methods=("GET", "POST"))
 def create():
+    # if submitted a create user request, create user and return to home
     if request.method == "POST":
         username = request.form['username']
         email = request.form['email']
@@ -90,3 +120,27 @@ def create():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template("create.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user, remember=True)
+            return redirect(url_for('index'))
+        # else:
+        #     flash("Invalid username or password.")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# admin is used to create users, delete users, edit user info, delete user posts, delete user post comments
+@app.route("/admin")
+def admin():
+    users = User.query.all()
+    return render_template("admin.html", users=users)
